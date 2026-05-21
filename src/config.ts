fbcc5ea788e7aa8ef660a14b00c949b567179025
Dev <dev@juveonlabs.com>
2026-05-21 12:38:21 +0530

    Add Web3Forms access key

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
   // Get your free Web3Forms access key at: https://web3forms.com
   // Once you get the key, simply replace the text below with your key.
-  WEB3FORMS_ACCESS_KEY: "YOUR_ACCESS_KEY_HERE"
+  WEB3FORMS_ACCESS_KEY: "46d48888-44e8-4f49-b431-1c71b9583eb5"
 };
